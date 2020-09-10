c6ba37924a40be8a05237ec008b90b8c3cb7a3ae
Gary Hake <Gary.Hake@waystar.com>
2020-09-10 15:55:27 -0400

    mongodb stitch (realm) done

---
+++ 18-MongoDB Realm/src/pages/Product/Products.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import { Stitch, RemoteMongoClient } from 'mongodb-stitch-browser-sdk';
+import BSON from 'bson';
+
+import Products from '../../components/Products/Products';
+
+/* const appId = "myshop-tizzo"; // Set Realm app ID here.
+const appConfig = {
+  id: appId,
+  timeout: 10000, // timeout in milliseconds
+}; */
+
+class ProductsPage extends Component {
+  state = { isLoading: true, products: [] };
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  productDeleteHandler = productId => {
+    const mongoClient = Stitch.defaultAppClient.getServiceClient(
+      RemoteMongoClient.factory, 'mongodb-atlas');
+      
+    mongoClient
+      .db('shop')
+      .collection('products')
+      .deleteOne({ _id: new BSON.ObjectId(productId) })
+      .then(result => {
+        console.log(result);
+        this.fetchData();
+      })
+      .catch(err => {
+        this.props.onError(
+          'Deleting the product failed. Please try again later'
+        );
+        console.log(err);
+      });
+  };
+
+  fetchData = () => {
+    /* const app = new Realm.app(appConfig);
+    const user = app.logIn(Realm.Credentials.anonymous());
+    const mongoClient = user.remoteMongoClient("mongodb-atlas"); */
+    var mongoClient = Stitch.defaultAppClient.getServiceClient(
+      RemoteMongoClient.factory, "mongodb-atlas");
+
+    mongoClient
+      .db('shop')
+      .collection('products')
+      .find()
+      .asArray()
+      .then(products => {
+        products.map(product => {
+          product._id = product._id.toString();
+          product.price = product.price.toString();
+
+          return product;
+        });
+        this.setState({isLoading: false, products: products});
+      })
+      .catch(err => {
+        this.setState({isLoading: false});
+        this.props.onError(
+          'Fetching products failed. Please try again later'
+        );
+        console.log(err);
+      });
+  };
+
+  render() {
+    let content = <p>Loading products...</p>;
+
+    if (!this.state.isLoading && this.state.products.length > 0) {
+      content = (
+        <Products
+          products={this.state.products}
+          onDeleteProduct={this.productDeleteHandler}
+        />
+      );
+    }
+    if (!this.state.isLoading && this.state.products.length === 0) {
+      content = <p>Found no products. Try again later.</p>;
+    }
+    return <main>{content}</main>;
+  }
+}
+
+export default ProductsPage;
